8bfbc785130be21c7482e23707f483418a4915fe
Jay-shingala <jayshingala003@gmail.com>
2024-09-25 12:06:52 +0530

    new line determine test

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -11,3 +11,9 @@ test('should return the number itself for a single number', () => {
 test('should return the sum for two numbers', () => {
   expect(add("1,2")).toBe(3);
 });
+
+
+// new line test case
+test('should handle new lines between numbers', () => {
+  expect(add("1\n2,3")).toBe(6);
+});
--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,4 +1,14 @@
+// basic function 
+
+// export function add(numbers: string): number {
+//     if (!numbers) return 0;
+//     return numbers.split(',').reduce((sum, num) => sum + Number(num), 0);
+// }
+
+// apply with multiple values with /n
+
+
 export function add(numbers: string): number {
     if (!numbers) return 0;
-    return numbers.split(',').reduce((sum, num) => sum + Number(num), 0);
+    return numbers.split(/[\n,]/).reduce((sum, num) => sum + Number(num), 0);
 }
