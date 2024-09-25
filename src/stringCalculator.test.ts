13f272bca24d9e96b39d590d3bb75abae614343d
Jay-shingala <jayshingala003@gmail.com>
2024-09-25 12:15:55 +0530

    change the delimiter

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -17,3 +17,7 @@ test('should return the sum for two numbers', () => {
 test('should handle new lines between numbers', () => {
   expect(add("1\n2,3")).toBe(6);
 });
+
+test('should handle custom delimiters', () => {
+  expect(add("//;\n1;2")).toBe(3);
+});
--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -8,7 +8,24 @@
 // apply with multiple values with /n
 
 
+// export function add(numbers: string): number {
+//     if (!numbers) return 0;
+//     return numbers.split(/[\n,]/).reduce((sum, num) => sum + Number(num), 0);
+// }
+
+
+
+//To change the delimiter, the beginning of the string will contain a separate line that looks like this: "//[delimiter]\n[numbers…]". For example, "//;\n1;2" where the delimiter is ";" should return 3.
+
 export function add(numbers: string): number {
     if (!numbers) return 0;
-    return numbers.split(/[\n,]/).reduce((sum, num) => sum + Number(num), 0);
+
+    let delimiter = /[\n,]/;
+    if (numbers.startsWith("//")) {
+        const parts = numbers.split("\n");
+        delimiter = new RegExp(`[${parts[0].slice(2)}]`);
+        numbers = parts[1];
+    }
+
+    return numbers.split(delimiter).reduce((sum, num) => sum + Number(num), 0);
 }
